a37323330e8b7d0ad5f104ec6836b9ace922d3e7
timkormachev <kormachev.t@gmail.com>
2020-07-26 16:41:44 +0300

    Hello World for Spring Boot

---
+++ src/test/java/ru/timkormachev/launchvote/LaunchvoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.timkormachev.launchvote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchvoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
